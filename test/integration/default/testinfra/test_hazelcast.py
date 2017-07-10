31af07b50a47416d6aa1aa782e031cc95799be08
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-07-10 15:33:57 +0100

    Add a test for the config file.

--- test/integration/default/testinfra/test_hazelcast.py
+++ test/integration/default/testinfra/test_hazelcast.py
@@ -4,3 +4,9 @@ import testinfra
 def test_is_hazelcast_downloaded(host):
     hazelcast = host.file('/opt/hazelcast-3.8.2')
     assert hazelcast.is_directory
+
+def test_config_file(host):
+    hazelcast = host.file("/opt/hazelcast-3.8.2/hazelcast.conf")
+    assert hazelcast.contains("5701")
+    assert hazelcast.contains("NQmdgfeYYq1FfSQWR6")
+
