276a129954ec8cd8c2cfa2c163ff5ee27119f840
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-07-11 12:46:27 +0100

    Add test for user and group

--- test/integration/default/testinfra/test_hazelcast.py
+++ test/integration/default/testinfra/test_hazelcast.py
@@ -1,9 +1,17 @@
 import testinfra
 
 
+def test_does_hazelcast_user_exist(host):
+    hazelcast = host.user('hazelcast')
+    assert hazelcast.name == 'hazelcast'
+
+def test_does_hazelcast_group_exist(host):
+    hazelcast = host.group('hazelcast')
+    assert hazelcast.exits
+
 def test_is_hazelcast_downloaded(host):
     hazelcast = host.file('/opt/hazelcast-3.8.2')
-    assert hazelcast.is_directory
+    assert hazelcast.is_directory    
 
 def test_config_file(host):
     hazelcast = host.file("/opt/hazelcast-3.8.2/hazelcast.conf")
